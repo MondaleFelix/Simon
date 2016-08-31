2f9512e5b76c624e0c910801664aa4841889a716
Mondale Felix <mondalefelix@yahoo.com>
2016-08-31 15:48:21 -0700

    testing commit

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 console.log("oranges");
-var code;
+var code = [];
 var score;
 var difficulty;
-var guesses;
+var guesses = [];
 // Sound Variables
 var colorSounds = [
   "sounds/greenSound.wav",
@@ -30,11 +30,12 @@ var greenSound = new Audio("sounds/greenSound.wav");
 
 function render(){
   $('#score').html(score);
+  $('#difficulty').html(difficulty);
 }
 
 var init = function(){
   score = 0;
-  difficulty = 300;
+  difficulty = 1;
   setCode();
   console.log(code);
   render();
@@ -85,50 +86,143 @@ function resetDisplay() {
 
 //handleGuess(colorIdx){
   //guesses.push(colorIdx);
- // }  // check if wrong or correct
+// }
+// lose();
 
+// function lose(){
+//   if (code === guesses)
+//     displayCode();
+//   else {
+//     alert('loser');
+//   }
+
+// }
+
+
+
+ // check if wrong or correct
+
+function checkWin(){
+  if (guesses.toString() === code.toString()) {
+    score = (score + (1 * difficulty)) ;
+    render();
+  } else
+    //alert("you fuckn lose");
+;}
 
 
 $("#middleButton").click(function(evt){
   guesses = [];
   displayCode();
+  score = 0;
+  render();
+});
+
+$("#blackCircle").on('click', ".simon-button", function(){
+  if ($(this).attr('id') === "greenButton"){
+    guesses.push(0);
+    greenSound.play();
+    $("#greenButton").attr("src", "images/greenClick.png");
+    setTimeout(function() {
+      $("#greenButton").attr("src", "images/greenButton.png");
+    }, 100);
+  }else if  ($(this).attr('id') === "redButton"){
+    guesses.push(1);
+    redSound.play();
+    $("#redButton").attr("src", "images/redClick.png");
+    setTimeout(function() {
+      $("#redButton").attr("src", "images/redButton.png");
+    }, 100);
+  }else if  ($(this).attr('id') === "blueButton"){
+    guesses.push(2);
+    blueSound.play();
+    $("#blueButton").attr("src", "images/blueClick.png");
+    setTimeout(function() {
+      $("#blueButton").attr("src", "images/blueButton.png");
+    }, 100);
+  }else if  ($(this).attr('id') === "yellowButton"){
+    guesses.push(3);
+    yellowSound.play();
+    $("#yellowButton").attr("src", "images/yellowClick.png");
+    setTimeout(function() {
+      $("#yellowButton").attr("src", "images/yellowButton.png");
+    }, 100);
+  }
+  easterEgg();
+  checkWin();
+
 });
 
 
-$("#redButton").click(function(click) {
-  redSound.play();
-  $("#redButton").attr("src", "images/redClick.png");
-  setTimeout(function() {
-    $("#redButton").attr("src", "images/redButton.png");
-  }, 100);
-//  handleGuess(1);
+          // This is the red button stuff
+// $("#redButton").click(function(click) {
+//   guesses.push(1);
+//   redSound.play();
+//   $("#redButton").attr("src", "images/redClick.png");
+//   setTimeout(function() {
+//     $("#redButton").attr("src", "images/redButton.png");
+//   }, 100);
+// //  handleGuess(1);
+// });
+
+//           // This is the green button stuff
+// $("#greenButton").click(function(click) {
+//   guesses.push(0);
+//   greenSound.play();
+//   $("#greenButton").attr("src", "images/greenClick.png");
+//   setTimeout(function() {
+//     $("#greenButton").attr("src", "images/greenButton.png");
+//   }, 100);
+//   //handleGuess(0);
+// });
+
+          // This is the yellow button stuff
+// $("#yellowButton").click(function(click) {
+//   guesses.push(3);
+//   yellowSound.play();
+//   $("#yellowButton").attr("src", "images/yellowClick.png");
+//   setTimeout(function() {
+//     $("#yellowButton").attr("src", "images/yellowButton.png");
+//   }, 100);
+//  // handleGuess(3);
+// });
+//           // This is the blue button stuff
+// $("#blueButton").click(function(click) {
+//   guesses.push(2);
+//   blueSound.play();
+//   $("#blueButton").attr("src", "images/blueClick.png");
+//   setTimeout(function() {
+//     $("#blueButton").attr("src", "images/blueButton.png");
+//   }, 100);
+//  // handleGuess(2);
+// });
+
+// This increases the difficulty and limits it to 10
+$("#plus").click(function(click) {
+  if (difficulty < 10){
+    difficulty++;
+    setCode();
+    render();
+    console.log(code);
+  }
+
+});
+// This decreases the difficulty and limits it to 1
+$("#minus").click(function(click) {
+  if (difficulty > 1){
+    difficulty--;
+    setCode();
+    render();
+    console.log(code);
+  }
 });
 
-$("#greenButton").click(function(click) {
-  greenSound.play();
-  $("#greenButton").attr("src", "images/greenClick.png");
-  setTimeout(function() {
-    $("#greenButton").attr("src", "images/greenButton.png");
-  }, 100);
-  //handleGuess(0);
-});
+var egg = [0,1,2,3];
 
-$("#yellowButton").click(function(click) {
-  yellowSound.play();
-  $("#yellowButton").attr("src", "images/yellowClick.png");
-  setTimeout(function() {
-    $("#yellowButton").attr("src", "images/yellowButton.png");
-  }, 100);
- // handleGuess(3);
-});
-
-$("#blueButton").click(function(click) {
-  blueSound.play();
-  $("#blueButton").attr("src", "images/blueClick.png");
-  setTimeout(function() {
-    $("#blueButton").attr("src", "images/blueButton.png");
-  }, 100);
- // handleGuess(2);
-});
+function easterEgg(){
+  if (guesses.toString() === egg.toString()) {
+    alert("something creative");
+  }
+}
 
 init();
